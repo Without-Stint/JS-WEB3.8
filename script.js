d0d4f7ed6efc34d58198043e7d242d939442928c
Without-Stint <Hangman90@mail.ru>
2019-06-05 01:23:31 +0300

    version2.0

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let divs = document.querySelectorAll('div');
 
 for (let i=0; i<divs.length; i++){
     divs[i].addEventListener('click', function(){
+        event.stopPropagation;
         console.log(this.getAnimations('id'))
     });
 }
