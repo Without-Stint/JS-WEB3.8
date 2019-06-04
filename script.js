9c526069a82e04417fce37413ae0a792072c8218
Without-Stint <Hangman90@mail.ru>
2019-06-05 00:51:25 +0300

    version1.0

---
+++ script.js
@@ -0,0 +1,8 @@
+let divs = document.querySelectorAll('div');
+
+
+for (let i=0; i<divs.length; i++){
+    divs[i].addEventListener('click', function(){
+        console.log(this.getAnimations('id'))
+    });
+}
