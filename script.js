ab6f9398cf331fde2c635232e4931801c6192119
Without-Stint <Hangman90@mail.ru>
2019-06-05 01:30:21 +0300

    version3.0

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 let divs = document.querySelectorAll('div');
-
+let link = document.querySelector('a');
 
 for (let i=0; i<divs.length; i++){
     divs[i].addEventListener('click', function(event){
         event.stopPropagation();
         console.log(this.getAttribute('id'))
     });
+}
+
+link.addEventListener('click', handleLinkClick)
+
+function handleLinkClick(event){
+    event.preventDefault();
 }
