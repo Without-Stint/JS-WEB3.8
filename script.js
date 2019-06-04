d66ef4e53cd26787395a422bc918bb358d014eb7
Without-Stint <Hangman90@mail.ru>
2019-06-05 01:33:45 +0300

    version4.0

--- script.js
+++ script.js
@@ -12,4 +12,12 @@ link.addEventListener('click', handleLinkClick)
 
 function handleLinkClick(event){
     event.preventDefault();
+    let div = divs[0]
+    if (div.style.display == 'none') {
+        div.style.display = 'flex'
+    } else {
+        div.style.display = 'none'
+    }
+
+    console.log(div.style.display)
 }
